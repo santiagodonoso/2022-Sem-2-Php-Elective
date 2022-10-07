b053786f91001b11c7c4af3b7eb744f8a0406968
Santiago Donoso <sand@kea.dk>
2022-10-07 11:25:27 +0200

    ...

--- view_about_us.php
+++ view_about_us.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  About us
-</body>
-</html>
+<?php
+$_title = 'About us';
+require_once __DIR__.'/comp_header.php';
+?>
+
+<main>
+  <h1>
+    About us
+  </h1>
+</main>
+
+
+<?php
+require_once __DIR__.'/comp_footer.php';
+?>
+
--- view_index.php
+++ view_index.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  X
-</body>
-</html>
+<?php
+$_title = 'Welcome to Real Estate';
+require_once __DIR__.'/comp_header.php';
+?>
+
+<main>
+  <h1>
+    HOME
+  </h1>
+</main>
 
 
-
-
-
-
+<?php
+require_once __DIR__.'/comp_footer.php';
+?>
 
