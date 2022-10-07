8e07f624173403f682c71e513772ee727f0bd704
Santiago Donoso <sand@kea.dk>
2022-10-07 10:27:54 +0200

    ...

--- _x.php
+++ _x.php
@@ -1,8 +1,20 @@
 <?php
 
+define('_USER_NAME_MIN_LEN', 2);
+define('_USER_NAME_MAX_LEN', 50);
+
 define('_ITEM_ADDRESS_MIN_LEN', 10);
 define('_ITEM_ADDRESS_MAX_LEN', 100);
 
+// ##############################
+function _validate_user_name(){
+  $error_message = 'user_name '._USER_NAME_MIN_LEN.' to '._USER_NAME_MAX_LEN.' characters';
+  if( ! isset($_POST['user_name'])){ _respond($error_message, 400); }
+  $_POST['user_name'] = trim($_POST['user_name']);
+  if(strlen($_POST['user_name']) < _USER_NAME_MIN_LEN){ _respond($error_message); }
+  if(strlen($_POST['user_name']) > _USER_NAME_MAX_LEN){ _respond($error_message); }
+  return $_POST['user_name'];
+}
 
 // ##############################
 function _validate_item_address(){
