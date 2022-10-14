41a14f3cefad13b111c2c2f676083c18b586c2b5
Santiago Donoso <sand@kea.dk>
2022-10-14 11:14:34 +0200

    ...

--- transaction.php
+++ transaction.php
@@ -1,21 +1,34 @@
 <?php
+
+// For every statement 1 rollback when you check
+// Always a rollback in the last catch
+// Tehre is only 1 commit when everything went OK
+
 require_once(__DIR__.'/db.php');
 try{
   $_db->beginTransaction();
-  $q = $_db->prepare(''); // First of 2 or more updates, inserts, or deletes
-  $q->execute();
-  
+  $query = $_db->prepare('UPDATE users 
+                          SET user_name=:user_name 
+                          WHERE user_id=:user_id');
+  $query->bindValue(':user_name', 'CCC');
+  $query->bindValue(':user_id', '3');
+  $query->execute();
   
   // if rowCount then it was updated
-  if( $q->rowCount() == 0 ){
-    $db->rollback();
+  if( $query->rowCount() == 0 ){
+    $_db->rollback();
     echo 'Could not update the user';
     exit();
   }
-  // This is the 2nd or 3rd.......
-  $q = $db->prepare(''); // update, insert, or delete
-  $q->execute();
-  if( $q->rowCount() == 0 ){
+
+
+  $query = $_db->prepare('UPDATE usersxxx 
+                          SET user_name=:user_name 
+                          WHERE user_id=:user_id');
+  $query->bindValue(':user_name', 'DDD');
+  $query->bindValue(':user_id', '4');
+  $query->execute();
+  if( $query->rowCount() == 0 ){
     $_db->rollback();
     echo 'this went wrong';
     exit();
